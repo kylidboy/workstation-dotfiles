08490741d06ac4ea739ebc87b0485acc3500295c
Kyli Bing <kylidboy@outlook.com>
2023-03-19 16:42:48 +0800

    feat: remove useless files & update kitty

--- dotfiles/config/nvim/lua/bufferline-config/init.lua
+++
@@ -1,3 +0,0 @@
-require("bufferline").setup{}
-vim.api.nvim_set_keymap('n', '<TAB>', ':BufferLineCycleNext', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':BufferLineCyclePrev', {noremap = true, silent = true})
